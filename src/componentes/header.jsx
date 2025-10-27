320256d9432d0166a233a48007d0d02be7822367
Anderson Santo <a.santo28092006@gmail.com>
2025-10-27 14:23:28 -0300

    correção no local das fotos

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <header>
             <div className="logo">
              <h2>Adote um Amigo </h2>
-             <img src="public/patas.png" alt="" />
+             <img src="src/img/patas.png" alt="" />
              </div>
                <h5 className="meio-header">Bem vindo ao Adote um amigo!</h5>
              <a className="botao-cnt" target="_blank" href="https://whatsapp.com//?helpref=uf_share">Contato</a>
